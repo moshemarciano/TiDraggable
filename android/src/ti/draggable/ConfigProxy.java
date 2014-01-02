1a09396d3a8e00aacfa66eb198d5d155415dcda0
Seth Benjamin <animecyc@gmail.com>
2014-01-02 17:56:43 -0500

    Fixing a bug where a draggable proxy with no initial configuration options would immediately cause an application to crash. Bumping android version.

--- android/src/ti/draggable/ConfigProxy.java
+++ android/src/ti/draggable/ConfigProxy.java
@@ -59,30 +59,20 @@ public class ConfigProxy extends KrollProxy implements KrollProxyListener
 	{
 		super();
 
-		Object[] maps = null;
+		properties.put("enabled", config != null && config.containsKeyAndNotNull("enabled") ? TiConvert.toBoolean(config, "enabled", true) : true);
+		properties.put("ensureRight", config != null && config.containsKeyAndNotNull("ensureRight") ? TiConvert.toBoolean(config, "ensureRight", false) : false);
+		properties.put("ensureBottom", config != null && config.containsKeyAndNotNull("ensureBottom") ? TiConvert.toBoolean(config, "ensureBottom", false) : false);
+		properties.put("minLeft", config != null && config.containsKeyAndNotNull("minLeft") ? TiConvert.toDouble(config, "minLeft") : null);
+		properties.put("maxLeft", config != null && config.containsKeyAndNotNull("minLeft") ? TiConvert.toDouble(config, "maxLeft") : null);
+		properties.put("minTop",config != null &&  config.containsKeyAndNotNull("minTop") ? TiConvert.toDouble(config, "minTop") : null);
+		properties.put("maxTop", config != null && config.containsKeyAndNotNull("maxTop") ? TiConvert.toDouble(config, "maxTop") : null);
+		properties.put("axis", config != null && config.containsKeyAndNotNull("axis") ? TiConvert.toString(config, "axis") : null);
+		properties.put("maps", config != null && config.containsKeyAndNotNull("maps") ? (Object[]) config.get("maps") : null);
 
-		if (config != null)
-		{
-			if (config.containsKeyAndNotNull("maps"))
-			{
-				maps = (Object[]) config.get("maps");
-			}
-		}
-
-		properties.put("enabled", TiConvert.toBoolean(config, "enabled", true));
-		properties.put("ensureRight", TiConvert.toBoolean(config, "ensureRight", false));
-		properties.put("ensureBottom", TiConvert.toBoolean(config, "ensureBottom", false));
-		properties.put("minLeft", config.containsKeyAndNotNull("minLeft") ? TiConvert.toDouble(config, "minLeft") : null);
-		properties.put("maxLeft", config.containsKeyAndNotNull("minLeft") ? TiConvert.toDouble(config, "maxLeft") : null);
-		properties.put("minTop", config.containsKeyAndNotNull("minTop") ? TiConvert.toDouble(config, "minTop") : null);
-		properties.put("maxTop", config.containsKeyAndNotNull("maxTop") ? TiConvert.toDouble(config, "maxTop") : null);
-		properties.put("axis", config.containsKeyAndNotNull("axis") ? TiConvert.toString(config, "axis") : null);
-		properties.put("maps", maps);
-
-		dimensions.put("minLeft", config.containsKeyAndNotNull("minLeft") ? TiConvert.toTiDimension(config, "minLeft", TiDimension.TYPE_LEFT) : null);
-		dimensions.put("maxLeft", config.containsKeyAndNotNull("minLeft") ? TiConvert.toTiDimension(config, "maxLeft", TiDimension.TYPE_LEFT) : null);
-		dimensions.put("minTop", config.containsKeyAndNotNull("minTop") ? TiConvert.toTiDimension(config, "minTop", TiDimension.TYPE_TOP) : null);
-		dimensions.put("maxTop", config.containsKeyAndNotNull("maxTop") ? TiConvert.toTiDimension(config, "maxTop", TiDimension.TYPE_TOP) : null);
+		dimensions.put("minLeft", config != null && config.containsKeyAndNotNull("minLeft") ? TiConvert.toTiDimension(config, "minLeft", TiDimension.TYPE_LEFT) : null);
+		dimensions.put("maxLeft", config != null && config.containsKeyAndNotNull("minLeft") ? TiConvert.toTiDimension(config, "maxLeft", TiDimension.TYPE_LEFT) : null);
+		dimensions.put("minTop", config != null && config.containsKeyAndNotNull("minTop") ? TiConvert.toTiDimension(config, "minTop", TiDimension.TYPE_TOP) : null);
+		dimensions.put("maxTop", config != null && config.containsKeyAndNotNull("maxTop") ? TiConvert.toTiDimension(config, "maxTop", TiDimension.TYPE_TOP) : null);
 
 		setModelListener(this);
 	}
